freek1e/device_xiaomi_duchamp

duchamp: Let's go full prebuilt for C2 vendor stack
That's the only sane way to make C2 not kill itself randomly.

Change-Id: I46d9f4fe1bf85db11b89ecfb2e2866272e4a2719

extract-files.py
@@ -73,8 +73,6 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/hw/mt6897/mapper.mediatek.so',
      'vendor/lib64/libaimemc.so',
      'vendor/lib64/libcodec2_fsr.so',
-     'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
-     'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
      'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V4-ndk.so',
@@ -123,6 +121,68 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libultrahdr-v34.so': blob_fixup()
         .replace_needed('libjpegencoder.so', 'libjpegencoder-v34.so')
         .replace_needed('libjpegdecoder.so', 'libjpegdecoder-v34.so'),
+
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v34.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v34.so')
+        .replace_needed('libcodec2_hidl@1.2.so', 'libcodec2_hidl@1.2-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so'),
+
+    'vendor/lib64/libcodec2_fsr.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.0-v34.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v35.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.1-v34.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v35.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v34.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.2-v34.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v35.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v34.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v34.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    ('vendor/lib64/libcodec2_hidl_plugin-v34.so',
+     'vendor/lib64/libsfplugin_ccodec_utils-v34.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so'),
+
+    ('vendor/lib64/libcodec2_mtk_c2store.so',
+     'vendor/lib64/libcodec2_mtk_vdec.so',
+     'vendor/lib64/libcodec2_mtk_venc.so',
+     'vendor/lib64/libcodec2_vpp_fa_plugin.so',
+     'vendor/lib64/libcodec2_vpp_mi_plugin.so',
+     'vendor/lib64/libcodec2_vpp_qt_plugin.so',
+     'vendor/lib64/libcodec2_vpp_rs_plugin.so'): blob_fixup()
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_soft_common-v34.so': blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    ('vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
+     'vendor/lib64/libcodec2_vpp_AISR_plugin.so'): blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v34.so'),
+
+    'vendor/lib64/libcodec2_vndk-v34.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so')
+        .replace_needed('android.hardware.media.bufferpool2-V1-ndk.so', 'android.hardware.media.bufferpool2-V2-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

Aayush787/android_device_xiaomi_duchamp

extract-files.py
@@ -94,7 +94,6 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/hw/mt6897/android.hardware.graphics.mapper@4.0-impl-mediatek.so',
      'vendor/lib64/hw/mt6897/mapper.mediatek.so',
      'vendor/lib64/libaimemc.so',
-     'vendor/lib64/libcodec2_fsr.so',
      'vendor/lib64/vendor.mediatek.hardware.camera.isphal-V1-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V4-ndk.so',
@@ -122,8 +121,71 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee'): blob_fixup()
         .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V3-ndk-v34.so'),
 
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v34.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v34.so')
+        .replace_needed('libcodec2_hidl@1.2.so', 'libcodec2_hidl@1.2-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so'),
+
     'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
         .regex_replace('.+dolby.+\n', ''),
+
+    'vendor/lib64/libcodec2_fsr.so': blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.0-v34.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v36.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.1-v34.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v36.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v34.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_hidl@1.2-v34.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v36.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v34.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v34.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    ('vendor/lib64/libcodec2_hidl_plugin-v34.so',
+     'vendor/lib64/libsfplugin_ccodec_utils-v34.so'): blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so'),
+
+    ('vendor/lib64/libcodec2_mtk_c2store.so',
+     'vendor/lib64/libcodec2_mtk_vdec.so',
+     'vendor/lib64/libcodec2_mtk_venc.so',
+     'vendor/lib64/libcodec2_vpp_fa_plugin.so',
+     'vendor/lib64/libcodec2_vpp_mi_plugin.so',
+     'vendor/lib64/libcodec2_vpp_qt_plugin.so',
+     'vendor/lib64/libcodec2_vpp_rs_plugin.so'): blob_fixup()
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    'vendor/lib64/libcodec2_soft_common-v34.so': blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v34.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+
+    ('vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so',
+     'vendor/lib64/libcodec2_vpp_AISR_plugin.so'): blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so')
+        .replace_needed('libcodec2_soft_common.so', 'libcodec2_soft_common-v34.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v34.so')
+        .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v34.so'),
+
+    'vendor/lib64/libcodec2_vndk-v34.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so')
+        .replace_needed('android.hardware.media.bufferpool2-V1-ndk.so', 'android.hardware.media.bufferpool2-V2-ndk.so'),
 
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService'),