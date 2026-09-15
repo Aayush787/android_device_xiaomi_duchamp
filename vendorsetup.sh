384638cf3e717fab414a851af4ee056a6f9febaa
Aayush787 <crazyaayush787@gmail.com>
2026-09-15 14:32:44 +0000

    duchamp: Add Lunaris Dolby Atmos Support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,5 @@
+echo "Cloning Dolby"
+git clone -b 16 https://github.com/Pong-Development/hardware_dolby.git hardware/dolby
+
 echo "Exporting prebuilt headers path"
 export TARGET_PREBUILT_KERNEL_HEADERS=device/xiaomi/duchamp-kernel/kernel-uapi-headers.tar.gz
