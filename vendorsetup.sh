c380b0ad2ebae4bd2c6d8f5bf8beb8aa2ba1bb38
Aayush787 <crazyaayush787@gmail.com>
2026-09-18 03:59:53 +0000

    duchamp: vendorsetup: Provide libbase-v34

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
+echo "Provide libbase-v34"
+cd hardware/lineage/compat
+git fetch https://github.com/mt6897-devs/hardware_lineage_compat.git
+git cherry-pick e167e38df9e0e8623566b006dfdca0dcc908becf
+cd ../../..
+
 echo "Cloning Dolby"
 git clone -b 16 https://github.com/Pong-Development/hardware_dolby.git hardware/dolby
 
