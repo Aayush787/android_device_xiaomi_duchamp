fdf4f06a9afb4186c33fcaed860c690d5a5823b0
Aayush787 <crazyaayush787@gmail.com>
2026-09-18 04:49:35 +0000

    duchamp: vendorsetup: Add Sign Keys & Fixups

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+echo "Clonning Private keys"
+croot && git clone -b 16 https://github.com/ProjectMatrixx/vendor_matrixx-priv_keys-template.git vendor/infinity-priv/keys
+cd vendor/infinity-priv/keys
+./keys.sh
+cd ../../..
+
+echo "Unlock 4K 60FPS Cam Recording"
+cd p*/a*/Aperture
+git fetch https://github.com/Nothing-2A/android_packages_apps_Aperture.git
+git cherry-pick 9509277efc852ad8bdcce204e0d9cfe104b6d190
+cd ../../..
+
+echo "Fixup! L2CAP and A2DP offload coex mechanism for MTK"
+cd packages/modules/Bluetooth
+git fetch https://github.com/mt6897-devs/packages_modules_Bluetooth.git
+git cherry-pick bea05a62eef1fcc37e5a10d476580a017a0bd32e
+cd ../../..
+
 echo "Provide libbase-v34"
 cd hardware/lineage/compat
 git fetch https://github.com/mt6897-devs/hardware_lineage_compat.git
